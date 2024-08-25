78510ff6ca1c95cc00cd0d109a7f9851c161ca22
mattywentworth <wentworthmm@gmail.com>
2024-08-25 11:08:48 -0400

    Nearly finish FeedTile styling

--- src/features/feed/FeedTile.jsx
+++ src/features/feed/FeedTile.jsx
@@ -30,16 +30,16 @@ export const FeedTile = () => {
                 */}
                 <div className={styles.feedTileRowFour}>
                     <div className={styles.voteInfo}>
-                        <img></img>
+                        <img className={styles.imgLeft} src='https://png.pngtree.com/png-vector/20230120/ourmid/pngtree-neon-square-frame-clipart-png-image_6568438.png'></img>
                         <p>vote ct</p>
-                        <img></img>
+                        <img className={styles.imgRight} src='https://png.pngtree.com/png-vector/20230120/ourmid/pngtree-neon-square-frame-clipart-png-image_6568438.png'></img>
                     </div>
                     <div className={styles.commentInfo}>
-                        <img></img>
+                        <img className={styles.imgLeft} src='https://png.pngtree.com/png-vector/20230120/ourmid/pngtree-neon-square-frame-clipart-png-image_6568438.png'></img>
                         <p>comment ct</p>
                     </div>
                     <div className={styles.shareInfo}>
-                        <img></img>
+                        <img className={styles.imgLeft} src='https://png.pngtree.com/png-vector/20230120/ourmid/pngtree-neon-square-frame-clipart-png-image_6568438.png'></img>
                         <p>Share</p>
                     </div>
                 </div>
